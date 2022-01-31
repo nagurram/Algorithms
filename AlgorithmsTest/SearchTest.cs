188393b742cf0fe2a53cfb94548a012bd2983497
Narendranath <naren.7090@outlook.com>
2022-01-31 11:50:59 +1100

    added unit test project

---
+++ AlgorithmsTest/SearchTest.cs
@@ -0,0 +1,18 @@
+using Algorithms.Search;
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class SearchTest
+    {
+        [Fact]
+        public void BinarySearchPass()
+        {
+            int[] arr = new int[] { 1, 2, 3, 5, 87, 98, 345, 7654, 30343, 54534534 };
+            Search objSearch = new Search();
+            int result=objSearch.BinarySearch(arr, 87);
+            Assert.Equal(result, 87);
+        }
+    }
+}
