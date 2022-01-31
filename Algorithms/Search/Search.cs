0b37e9df0e6b9d6f0e9eccb7f74d0eb0443c5046
Narendranath <naren.7090@outlook.com>
2022-01-31 11:32:18 +1100

    Binary Search added

---
+++ Algorithms/Search/Search.cs
@@ -0,0 +1,33 @@
+﻿namespace Algorithms.Search
+{
+    public class Search
+    {
+        public int BinarySearch(int[] intarr, int searchnum)
+        {
+            int result = 0;
+            int mid = 0;
+            int low = 0;
+            int high = intarr.Length;
+            while (low <= high)
+            {
+                mid = low + ((high - low) / 2);
+                if (intarr[mid] == searchnum)
+                {
+                    return searchnum;
+                }
+                else
+                {
+                    if (intarr[mid] < searchnum)
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        high = mid - 1;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
